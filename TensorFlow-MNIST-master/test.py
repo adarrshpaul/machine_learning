1219b42bd888c0e8c3d15dc2aa94c1feac99e656
Adarrsh Paul <pauladarrsh@gmail.com>
2018-06-29 11:31:02 +0530

    TensorFlow-MNIST-master

---
+++ TensorFlow-MNIST-master/test.py
@@ -0,0 +1,31 @@
+import os
+import tkinter
+from MNISTTester import MNISTTester
+
+####################
+# directory settings
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+data_path = script_dir + '/mnist/data/'
+model_path = script_dir + '/models/mnist-cnn'
+
+#####################################
+# prediction test with MNIST test set
+mnist = MNISTTester(
+            model_path=model_path,
+            data_path=data_path)
+
+mnist.accuracy_of_testset()
+mnist.predict_random()
+
+#################################
+# prediction test with image file
+# mnist = MNISTTester(model_path)
+mnist.predict(script_dir + '/imgs/digit-4.jpg')
+mnist.predict(script_dir + '/imgs/digit-2.jpg')
+mnist.predict(script_dir + '/imgs/digit-5.jpg')
+mnist.predict(script_dir + '/imgs/digit-1.jpg')
+mnist.predict(script_dir + '/imgs/digit-3.jpg')
+
+
+
